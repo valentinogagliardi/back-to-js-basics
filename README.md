748c72d12f3ab0b9df021d3fe5ccdd0d7ff2fb1c
Valentino Gagliardi <valentino@valentinog.com>
2019-02-20 11:49:53 +0100

    Added: build table

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# back-to-js-basics
+# Back to JavaScript basics
+> A collection of vanilla JavaScript exercises
+
+## Tutorials
+
+[Back To The Basics: How To Generate a Table With JavaScript](https://www.valentinog.com/blog/html-table/)
---
+++ build-table/build-table.js
@@ -0,0 +1,34 @@
+let mountains = [
+  { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+  { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+  { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Amiata", height: 1738, place: "Siena" }
+];
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(mountains[0]);
+generateTable(table, mountains);
+generateTableHead(table, data);
